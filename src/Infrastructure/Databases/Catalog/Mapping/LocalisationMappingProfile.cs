afb3ad9fec4a36e363c40aadc94c148d9f7d3ffb
Madowwl <emma.arlanda@viacesi.fr>
2024-09-16 21:11:15 +0200

    feat: map profiles

--- src/Infrastructure/Databases/Catalog/Mapping/LocalisationMappingProfile.cs
+++ src/Infrastructure/Databases/Catalog/Mapping/LocalisationMappingProfile.cs
@@ -6,4 +6,17 @@ using Infrastructure = Models;
 
 internal class LocalisationMappingProfile : Profile
 {
+    public LocalisationMappingProfile()
+    {
+        _ = this.CreateMap<Application.City, Infrastructure.City>()
+            .ForMember(d => d.DateCreated, o => o.Ignore())
+            .ForMember(d => d.DateModified, o => o.Ignore())
+            .ReverseMap();
+
+        _ = this.CreateMap<Application.Region, Infrastructure.Region>()
+            .ForMember(d => d.DateCreated, o => o.Ignore())
+            .ForMember(d => d.DateModified, o => o.Ignore())
+            .ForMember(d => d.Cities, o => o.Ignore())
+            .ReverseMap();
+    }
 }
